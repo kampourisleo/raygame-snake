b50befcd501780693501370f0655f34df67d8406
kampourisleo <kampleonidas@gmail.com>
2024-08-04 13:36:33 +0300

    Added fruit appearing and eating.

--- game.c
+++ game.c
@@ -6,7 +6,6 @@
 #define PHYSAC_IMPLEMENTATION
 #include "physac.h"
 
-
 int main()
 {   //RESOLUTION: 
     float screenWidth = 500.0;
@@ -28,26 +27,25 @@ int main()
     float radius;
     int scoreCounter = 0; 
     //For circle: 
-    int rand_x; 
-    int rand_y;
+    float rand_x; 
+    float rand_y;
     int lower_x = 1; 
     int lower_y = 1; 
-    int upper_x = 20; 
-    int upper_y = 20;
-
-
-
+    int upper_x = 19; 
+    int upper_y = 19; 
+    int circleExists = 0; 
+    int snakeSize = 0;
+//-----------------------------
     InitWindow(screenWidth, screenHeight, "raygame");
-
-    
-    
+//-----------------------------
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         BeginDrawing(); 
+        //WINDOW AND COLOR CHANGE
         Color clearColor = BLACK;
         if (IsKeyDown(KEY_SPACE)){ 
-            clearColor = GREEN;
+            clearColor = PINK;
         } 
         ClearBackground(clearColor);
         //Drawing lines to form tile grid on screen-space.
@@ -57,18 +55,25 @@ int main()
         for (int i=1; i <= (screenWidth/tileWidth - 1); i++){ 
             DrawLine(i*tileWidth, 0, i*tileWidth, screenHeight, WHITE);
         }
-
+        //---------------------------------
         //GENERATING RANDOM CIRCLES PART: 
         //TILE SIZE: RECTANGLE HEIGHT X RECTANGLE WIDTH. ==> 
         //CENTER: y2 = y + tileHeight/2, x2 = x + tileWidth/2. 
         //Circle RANDOM position: 
-        rand_x = ((rand() % (upper_x - lower_x + 1)  + lower_x) * tileWidth) + tileWidth/2; 
-        rand_y = ((rand() % (upper_y - lower_y + 1) + lower_y) * tileHeight) + tileHeight/2; 
-        //Radius:
-        if(tileHeight >= tileWidth) radius = tileWidth; 
-        else radius = tileHeight;
+        if (circleExists==0)
+        {
+            rand_x = ((rand() % (upper_x - lower_x + 1)  + lower_x) * tileWidth) + tileWidth/2; 
+            rand_y = ((rand() % (upper_y - lower_y + 1) + lower_y) * tileHeight) + tileHeight/2; 
+            //Radius:
+            if(tileHeight >= tileWidth) radius = tileWidth/2; 
+            else radius = tileHeight/2;
+            //Drawing circle: 
+            DrawCircle(rand_x, rand_y, (radius), RED); 
+            circleExists = 1;
+        } 
         //Drawing circle: 
-        DrawCircle(rand_x, rand_y, (radius/2), RED);
+        DrawCircle(rand_x, rand_y, (radius), RED); 
+        //----------------------------------------------
         //"Use 'WASD' to move player" 
         //DRAW SNAKE HEAD, MOVEMENT INCREMENTS AND BORDER CHECK PART:  
         DrawRectangle(x1, y1, RectangleWidth, RectangleHeight, SKYBLUE); //DRAWING STARTS FROM UPPER LEFT CORNER.
@@ -85,7 +90,13 @@ int main()
             y1 = y1; 
             x1 = x1 + (RectangleWidth);
         }
-           
+        //---------------------------------- 
+        //EATING CIRCLE (FRUIT) PART:
+        if((x1 == (rand_x - tileWidth/2)) && (y1 == (rand_y - tileHeight/2))){ 
+        circleExists=0;
+
+        }
+
         //--------------------------
         EndDrawing();
     }
